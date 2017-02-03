2c3da7abbd3eb138266551f1c75b7c63de644333
Rahul Kadyan <hi@znck.me>
2017-02-03 12:21:38 +0530

    Set root scope to have keys

--- src/Traits/TransformResponse.php
+++ src/Traits/TransformResponse.php
@@ -5,6 +5,6 @@ use Znck\Transformers\Transformer;
 trait TransformResponse
 {
     public function callAction($method, $parameters) {
-        return Transformer::response(parent::callAction($method, $parameters));
+        return Transformer::response(parent::callAction($method, $parameters), null, null, true);
     }
 }
